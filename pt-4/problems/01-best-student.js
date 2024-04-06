3ce335c4a52f78a4f403dd628f20bcfd8e4eee4f
Omega Dziko F. <megaoy@yahoo.com>
2024-04-06 17:21:56 -0400

    pt4 1st prb slvd

--- pt-4/problems/01-best-student.js
+++ pt-4/problems/01-best-student.js
@@ -1,5 +1,5 @@
 /*
-    
+
     Write a function bestStudent() that takes any number of student objects, each with names for keys, and total points for values.
     The function should return the name of the student with the most points.
     If no students are passed to the function, return null;
@@ -13,11 +13,37 @@
 
     console.log(bestStudent(student1, student2, student3, student4, student5)) // returns "Justin"
 
-    
+
 */
 
 const bestStudent = (...args) => {
-    // Your code here 
+
+    //set conditional to return null if there is no obj
+    if(args.length === 0) {
+
+        return null;
+
+    };
+
+    //set first obj of arr as best student
+    let bestStudent = args[0];
+
+    //loop through given arr of students
+    for (let student of args) {
+
+    //ask if curr student score is greater than best score
+        if(student.points > bestStudent.points) {
+
+    //if yes assign curr student as best
+            bestStudent = student;
+
+        }
+
+    }
+
+    //return best student name
+    return bestStudent.name;
+    
 }
 
 
